468a8cf1b0eb6ea14c3d2ba0a06cbcc3c118e8be
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-11-11 15:47:23 +0300

    search form

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+import { useAppDispatch } from '../app/hooks'
+import { getBooks } from '../features/booksSlice'
+
+const SearchForm = () => {
+    const [bookName,setBookName]=useState<string>("")
+    const dispatch=useAppDispatch()
+
+    const handleSearch=(e:any)=>{
+        e.preventDefault()
+        dispatch(getBooks(bookName))
+        setBookName("")
+    }
+
+  return (
+    <form 
+    className='max-w-[40rem] m-auto text-center p-10'
+    onSubmit={handleSearch}>
+        <input 
+        type="text"
+        value={bookName}
+        onChange={(e)=>setBookName(e.target.value)}
+        className="border border-slate-400 w-8/12 p-1"
+        placeholder='Please type book name here'
+        />
+        <button
+        type='submit'
+        className='bg-teal-500 w-3/12 sm:w-2/12 py-[5px] font-semibold'
+        >SEARCH</button>
+    </form>
+  )
+}
+
+export default SearchForm
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,16 @@
 import {useState,useEffect} from 'react'
-import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { useAppSelector } from '../app/hooks'
+import SearchForm from '../components/SearchForm'
 import SingleBook from '../components/SingleBook'
-import { getBooks } from '../features/booksSlice'
 import { Item } from '../types/interfaces'
 
 const Home = () => {
-  const dispatch=useAppDispatch()
-  const [bookName,setBookName]=useState<string>("crimeandpunishment")
-
   const {books,loading}=useAppSelector(state=>state.books)
 
-
-  useEffect(() => {
-    dispatch(getBooks(bookName))
-  }, [])
-  
-// console.log("books",books);
-// console.log("book",book);
   
   return (
+    <> 
+    <SearchForm/>
     <div className='container m-auto flex flex-wrap items-center justify-center gap-6'>
     {
       loading ? (
@@ -32,6 +24,8 @@ const Home = () => {
       )
     }
     </div>
+    </>
+
   )
 }
 
